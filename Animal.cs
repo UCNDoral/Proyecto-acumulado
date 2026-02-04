6855e129162ef1279633c545416c6fa5b55fb6ae
El-senpai <juiolechadog06@gmail.com>
2026-02-04 14:16:34 -0600

    se agrego la clase animal

---
+++ Animal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAcumulado
+{
+    internal class Animal
+    {
+    }
+}
