54e3f8d2dc4dd16bc57f44dfe465f07db23f7a71
Angel Lars <angel.larios@ucn.edu.ni>
2026-02-04 14:23:22 -0600

    se creo la instancia perro!

--- Program.cs
+++ Program.cs
@@ -10,7 +10,13 @@ namespace ProyectoAcumulado
     {
         static void Main(string[] args)
         {
+            Animal Perro = new Animal("Porqui", 25.5);
+
+
+            Console.WriteLine(Perro.nombre);
             Console.WriteLine("Este es mi proyecto");
+
+            Console.ReadLine();
         }
     }
 }
