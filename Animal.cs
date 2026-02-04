6331c85846c7710e8705be3b9c259bafa5c53a48
El-senpai <juiolechadog06@gmail.com>
2026-02-04 14:18:31 -0600

    se agrego la clase animal

--- Animal.cs
+++ Animal.cs
@@ -8,5 +8,31 @@ namespace ProyectoAcumulado
 {
     internal class Animal
     {
+        public string nombre {  get; set; }
+        public string edad {  get; set; }
+        public double peso { get; set; }
+
+        public Animal(string nombre , double peso)
+        {
+            this.nombre = nombre;
+            this.peso = peso;
+        }
+        public void sonido()
+        {
+
+            Console.WriteLine("el animal hace un sonido");
+
+
+        }
+
     }
+
+
+
+
+
+
+
+
+
 }
