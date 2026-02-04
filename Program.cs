5a31d54ba97fd9296662ea65d113402f24ee3017
Angel Larios <angel.larios@ucn.edu.ni>
2026-02-04 13:55:43 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoAcumulado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este es mi proyecto");
+        }
+    }
+}
